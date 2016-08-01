15bc9e279f21d6cd9076569e5ab4bd99fd05e323
momo_lichen <lc8681@gmail.com>
2016-08-01 19:00:17 +0800

    appium

---
+++ selendroid_demo.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# python学习，加油！
+import sys
+from sys import argv
+import os
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchAttributeException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from appium import webdriver
+from time import sleep
+
+
+PATH = lambda p: os.path.abspath(
+    os.path.join(os.path.dirname(__file__), p)
+)
+desired_caps = {}
+desired_caps['deviceName'] = '860BCML225ZG'  # adb devices查到的设备名
+desired_caps['platformName'] = 'Android'
+desired_caps['platformVersion'] = '5.1'
+desired_caps['automationName'] = 'Selendroid'  # Selendroid模式
+desired_caps['appPackage'] = 'com.immomo.momo'  # 被测App的包名
+desired_caps['appActivity'] = 'com.immomo.momo.android.activity.WelcomeActivity'  # 启动时的Activity
+wd = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
