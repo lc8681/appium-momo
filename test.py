15a1f7bd9f6bbe8d6ec2f31657b1b81c7c433f60
momo_lichen <lc8681@gmail.com>
2017-01-16 20:30:41 +0800

    appium

--- test.py
+++ test.py
@@ -19,58 +19,57 @@ class LoginTest(unittest.TestCase):
         sleep(1)
         # 点击登录按钮
         main.get_resource_id("visitor_btn_login").click()
-        print "login pass"
+        print "goto_login pass"
 
     def test_login(self):
-        login_again = True
-        while login_again:
+        momoid = main.get_resource_id("login_et_momoid")
+        mmpassword = main.get_resource_id("login_et_pwd")
+        def username_clear(text):
+            android_caps.self.keyevent(123)
+            for i in range(0, len(text)):
+                android_caps.self.keyevent(67)
+
+        momoid.click()
+        context2 = momoid.get_attribute('text')
+        username_clear(context2)
+
+        username = main.read_momoid()
+        # 输入用户名
+        momoid.send_keys(username)
+
+        mmpassword.click()
+        sleep(1)
+        android_caps.self.press_keycode(67)    # 删除键(方法太笨了..但是又想不到新方法,因为密码框没有text属性,所以不能和用户名一样处理)
+        android_caps.self.press_keycode(67)    # 67是keycode键值的回退删除
+        android_caps.self.press_keycode(67)
+        android_caps.self.press_keycode(67)
+        android_caps.self.press_keycode(67)
+        android_caps.self.press_keycode(67)
+        android_caps.self.press_keycode(67)
+        android_caps.self.press_keycode(67)
+        android_caps.self.press_keycode(67)
+        android_caps.self.press_keycode(67)
+        android_caps.self.press_keycode(67)
+
+        password = main.read_momo_password()
+        mmpassword.send_keys(password)     # 输入密码
+        main.get_resource_id("btn_ok").click()  # 确定登录
+        sleep(5)
+        try:
+            main.get_resource_id("like_match_bg_layout").is_displayed()
+            print "login pass"
+        except:
             try:
-                main.get_resource_id("login_et_momoid").is_displayed()
-                def username_clear(text):
-                    android_caps.self.keyevent(123)
-                    for i in range(0, len(text)):
-                        android_caps.self.keyevent(67)
-                adr = android_caps.self.find_element_by_id("login_et_momoid")
-                adr.click()
-                context2 = adr.get_attribute('text')
-                username_clear(context2)
-
-                username = adr.send_keys()
-                android_caps.self.find_element_by_id("login_et_momoid").send_keys(username)  # 输入用户名
-                android_caps.self.find_element_by_id("login_et_pself").click()
-                sleep(1)
-                android_caps.self.press_keycode(67)    # 删除键(方法太TM的笨了..但是又想不到新方法,因为密码框没有text属性,所以不能和用户名一样处理)
-                android_caps.self.press_keycode(67)    # 67是keycode键值的回退删除
-                android_caps.self.press_keycode(67)
-                android_caps.self.press_keycode(67)
-                android_caps.self.press_keycode(67)
-                android_caps.self.press_keycode(67)
-                android_caps.self.press_keycode(67)
-                android_caps.self.press_keycode(67)
-                android_caps.self.press_keycode(67)
-                android_caps.self.press_keycode(67)
-                android_caps.self.press_keycode(67)
-
-                password = raw_input("请输入密码:")
-                android_caps.self.find_element_by_id("login_et_pself").send_keys(password)     # 输入密码
-                android_caps.self.find_element_by_id("btn_ok").click()   # 确定登录
-                sleep(5)
-                try:
-                    android_caps.self.find_element_by_id("nearby_match_like").is_displayed()
-                    login_again = False
-                except:
-                    print ("用户名或密码错误,正在重启登录流程....")
-                    android_caps.self.find_element_by_id("button2").click()
-
+                main.get_resource_id("nearby_match_like").is_displayed()
+                print "login pass"
             except:
-                print ("用户名或密码错误,正在重启登录流程....")
-                android_caps.self.find_element_by_id("button2").click()
-
-        print("登录成功")
+                print "login fail"
+                android_caps.self.quit()
 
 if __name__ == '__main__':
     suite = unittest.TestSuite()
     suite.addTest(LoginTest('test_goto_login'))
+    suite.addTest(LoginTest('test_login'))
     timestr = time.strftime('%Y-%m-%d %X',time.localtime(time.time()))
     filename ='Test report'+'('+timestr+')'+'.html'
     fp = open(filename,'wb')
