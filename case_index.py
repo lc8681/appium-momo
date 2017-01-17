5a82a4e096bad69f41d61003a5567e20d45eabc9
momo_lichen <lc8681@gmail.com>
2017-01-17 16:01:22 +0800

    appium

--- case_index.py
+++ case_index.py
@@ -4,28 +4,26 @@ import time
 import unittest
 
 import HTMLTestRunner
-
 import android_caps
 import login
-import quanzi_index
 
 
 # 测试用例集合
 class LoginTest(unittest.TestCase):
-    def quit(self):
+    def quit(cls):
         android_caps.self.quit()
 
     def test_login(self):
         login
-
+'''
     def test_quanzi_index(self):
         quanzi_index
-
+'''
 
 if __name__ == '__main__':
     suite = unittest.TestSuite()
     suite.addTest(LoginTest('test_login'))
-    suite.addTest(LoginTest('test_quanzi_index'))
+   # suite.addTest(LoginTest('test_quanzi_index'))
     timestr = time.strftime('%Y-%m-%d %X', time.localtime(time.time()))
     filename = 'Test report' + '(' + timestr + ')' + '.html'
     fp = open(filename, 'wb')
